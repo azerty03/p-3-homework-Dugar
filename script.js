bb6c02879b03ba2861890751ca8096a2e9266a7e
Dugar <typical.god@mail.ru>
2025-04-29 17:08:33 +0800

    created index.html and scropt.jss

---
+++ script.js
@@ -0,0 +1,11 @@
+const myName = "Дугар";
+const yearOfBirth = 1997;
+const occupation = "бездельник";
+const city = "Улан-Удэ";
+console.log(myName);
+console.log(yearOfBirth);
+console.log(occupation);
+console.log(city);
+let bio = `Привет мир! Меня зовут ${myName}. Мне ${2025 - yearOfBirth} лет. Я живу в городе ${city} и хочу стать программистом!
+Я сам по себе ${occupation}, люблю онлайн игры. Они мои враги, если бы не они, давно стал крутым верстальщиком! xD`
+console.log(bio);
